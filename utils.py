b181e2bfbea6236221b74fa5b24bb0790796b47e
Chris Tozer <christopherjtozer@gmail.com>
2017-08-28 22:57:50 +0100

    Fix up config merge 2

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 def merge_dicts(defaults, overrides):
-    """Merges defaults and overrides.  overrides cannot define new keys not in defaults"""
     if isinstance(defaults, dict) and isinstance(overrides, dict):
         for k,v in overrides.items():
             if k not in defaults:
                 defaults[k] = v
             else:
                 defaults[k] = merge_dicts(defaults[k], v)
-    return defaults
+        return defaults
+    else:
+        return overrides
