f1b0ac52ac76e7a67ac97b6bf22445bf8deed857
Chris Tozer <christopherjtozer@gmail.com>
2017-08-28 23:25:37 +0100

    Update install script

--- install.py
+++ install.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 import sh
+import sys
 import os.path
-from lightup import Lightup
 
 # TODO Check that sh is installed and print a nice error if not.
 
-# Git updates:
-### On initial install: `git submodule init`
-### Every time get latest: `git submodule update --remote lightup-py`
+LIGHTUP_DIR="lightup"
+
+# On initial install: `git submodule init` to get main scripts
+git = sh.git.bake(_cwd=os.path.dirname(__file__))
+print(git.submodule.update("--init", "--recursive", LIGHTUP_DIR))
+
+# Now use the code to install itself!
+sys.path.insert(0, LIGHTUP_DIR)
+from lightup import Lightup
 
 l = Lightup()
 l.install()
