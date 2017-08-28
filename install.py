265f48646a72af99c992443ad8e261de8e9611e1
Chris Tozer <christopherjtozer@gmail.com>
2017-08-28 18:10:33 +0100

    Need sudo to create the softlink

--- install.py
+++ install.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import sh
+import os.path
 
-# Link lightup.pt to /etc/cron.hourly
-sh.ln("-s", "lightup.py", "/etc/cron.hourly")
+# Link lightup.py to /etc/cron.hourly
+script_path = os.path.join(os.path.abspath("./lightup.py"))
+if not os.path.exists(script_path):
+    raise Exception("%s doesn't exist" % script_path)
+print(script_path)
+
+with sh.contrib.sudo:
+    sh.ln("-s", script_path, "/etc/cron.hourly/lightup.py")
